29887ff90c005c86ce555ba04b649683841e22b3
JaeminMok <bamer@naver.com>
2023-02-22 16:15:00 +0900

    DFS_BFS

--- Review_TCT/3_DFS_BFS_Question4.py
+++ Review_TCT/3_DFS_BFS_Question4.py
@@ -3,14 +3,12 @@
 
 # 연구소
 
-
-
-
-# dfs부터 수정!!!!!!!!!!!!!!!!!!!!!!!!
-
+# 2가 왜 안퍼지는지 해결하지 못함...
 
 
 from itertools import combinations
+
+
 n = 7
 m = 7
 graph = [
@@ -22,6 +20,7 @@ graph = [
     [0, 1, 0, 0, 0, 0, 0],
     [0, 1, 0, 0, 0, 0, 0]
 ]
+
 # 벽을 놓을 위치들 
 emp = []
 for i in range(n):
@@ -29,23 +28,21 @@ for i in range(n):
         if graph[i][j] == 0:
             emp.append([i, j])
 
+
+# 방향
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
+
+
+# dfs
 def dfs(x, y):
-    if x+1 < n:
-        if graph[x+1][y] == 0:
-            graph[x+1][y] = 2
-            dfs(x+1, y)
-    if x-1 >= 0:
-        if graph[x-1][y] == 0:
-            graph[x-1][y] = 2
-            dfs(x-1, y)
-    if y+1 < m:
-        if graph[x][y+1] == 0:
-            graph[x][y+1] = 2
-            dfs(x, y+1)
-    if y-1 >= 0:
-        if graph[x][y-1] == 0:
-            graph[x][y-1] = 2
-            dfs(x, y-1)
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+        if nx >= 0 and nx < n and ny >= 0 and ny < m:
+            if graph[nx][ny] == 0:
+                graph[nx][ny] = 2
+                dfs(nx, ny)
 
 # 결과 모음
 result = []
@@ -53,38 +50,44 @@ result = []
 
 # 벽을 놓을 수 있는 모든 조합
 walls = list(combinations(emp, 3))
-# i = [[1, 0], [0, 1], [3, 5]]
 
 
-for i in walls:
-    # 벽 생성
-    for j in i:
-        x, y = j
-        graph[x][y] = 1
-
-    # 최초의 빈공간 찾기
-    for k in range(n):
-        for u in range(m):
-            if graph[k][u] == 2:
-                dfs(k, u)
-
-    # 그래프 출력
-    count = 0 
-    for i in range(len(graph)):
-        for m in graph[i]:
-            if m == 0:
-                count += 1
-    result.append(count)
-
+# for i in walls:
     # 그래프 재설정
-    graph = [
-        [2, 0, 0, 0, 1, 1, 0],
-        [0, 0, 1, 0, 1, 2, 0],
-        [0, 1, 1, 0, 1, 0, 0],
-        [0, 1, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 1, 1],
-        [0, 1, 0, 0, 0, 0, 0],
-        [0, 1, 0, 0, 0, 0, 0]
-    ]
+i = [[0, 1], [0, 2], [1, 0]]
+graph = [
+    [2, 0, 0, 0, 1, 1, 0],
+    [0, 0, 1, 0, 1, 2, 0],
+    [0, 1, 1, 0, 1, 0, 0],
+    [0, 1, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 1, 1],
+    [0, 1, 0, 0, 0, 0, 0],
+    [0, 1, 0, 0, 0, 0, 0]
+]
 
-print(result)
+# 벽 생성
+for j in i:
+    x, y = j
+    graph[x][y] = 1
+
+# 2찾고 퍼뜨리기
+for a in range(n):
+    for b in range(m):
+        if graph[a][b] == 2:
+            dfs(a, b)
+
+# 그래프에서 0 찾기
+count = 0 
+for k in range(len(graph)):
+    for m in graph[k]:
+        if m == 0:
+            count += 1
+result.append(count)
+
+# if count == 32:
+#     print(i)
+for a in range(len(graph)):
+    print(graph[a])
+    # break
+
+print(max(result))
