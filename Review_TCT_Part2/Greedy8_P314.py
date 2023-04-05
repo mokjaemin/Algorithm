4417d05d7f677b9c042373f2a1dbfb96c4368e0c
JaeminMok <bamer@naver.com>
2023-04-05 20:40:57 +0900

    Greedy

---
+++ Review_TCT_Part2/Greedy8_P314.py
@@ -0,0 +1,15 @@
+
+
+# 만들 수 없는 금액
+
+input = [3, 2, 1, 1, 9]
+input.sort()
+max_sum = 0
+for i in input:
+    max_sum += i
+for j in range(1, max_sum):
+    if j in input:
+        continue
+    else:
+        for k in 
+            
