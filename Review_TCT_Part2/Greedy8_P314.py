1f6cc53948e2d02df6bc5a603a412566a93c92f2
JaeminMok <bamer@naver.com>
2023-04-05 22:46:40 +0900

    Greedy

--- Review_TCT_Part2/Greedy8_P314.py
+++ Review_TCT_Part2/Greedy8_P314.py
@@ -2,14 +2,12 @@
 
 # 만들 수 없는 금액
 
-input = [3, 2, 1, 1, 9]
-input.sort()
-max_sum = 0
-for i in input:
-    max_sum += i
-for j in range(1, max_sum):
-    if j in input:
-        continue
-    else:
-        for k in 
-            
+num = [3, 2, 1, 1, 9]
+num.sort()
+target = 1
+for i in num:
+    if target < i:
+        break
+    target += i
+print(target)
+
