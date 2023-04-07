e9c36ac1f95ebb2b485336b02e7f5e1ff6c6207f
JaeminMok <bamer@naver.com>
2023-04-07 23:32:15 +0900

    test

--- Review_TCT_Part2/Greedy8_P314.py
+++ Review_TCT_Part2/Greedy8_P314.py
@@ -2,27 +2,11 @@
 
 # 만들 수 없는 금액
 
-# num = map(int, input("입력 : "))
-# num.sort()
-# target = 1
-# for i in num:
-#     if target < i:
-#         break
-#     target += i
-# print(target)
-
-
-def solution(players, callings):
-    for i in range(len(callings)):
-        for j in range(len(players)):
-            if players[j] == callings[i]:
-                if j-1 >= 0:
-                    players[j-1], players[j] = players[j], players[j-1]
-                break
-    return players
-
-
-players = ["mumu", "soe", "poe", "kai", "mine"]
-callings = ["kai", "kai", "mine", "mine"]
-result = ['mumu', 'kai', 'mine', 'soe', 'poe']
-print(solution(players, callings))
+num = map(int, input("입력 : "))
+num.sort()
+target = 1
+for i in num:
+    if target < i:
+        break
+    target += i
+print(target)
