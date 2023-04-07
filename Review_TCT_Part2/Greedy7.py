137d5594cea48f06e4c2e88ae9f1d3e5018f464c
JaeminMok <bamer@naver.com>
2023-04-07 23:28:44 +0900

    Test

--- Review_TCT_Part2/Greedy7.py
+++ Review_TCT_Part2/Greedy7.py
@@ -6,9 +6,11 @@ result = []
 index0 = 0
 index1 = 0
 for i in range(len(input)):
+    
     if i == 0:
         result.append(input[i])
         continue
+
     if result[-1] == input[i]:
         result.append(input[i])
     else:
