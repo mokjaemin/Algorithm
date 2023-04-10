9b084a9ae5d8c2b46a22896389fd648ee011d573
JaeminMok <bamer@naver.com>
2023-04-10 13:20:16 +0900

    DFS AND BFS

--- Memo.py
+++ Memo.py
@@ -11,3 +11,19 @@ print(test.isalpha())
 test = [1, 2, 3]
 test.pop(2)
 print(test)
+
+# 조합
+# a리스트에서 m개를 골라 조합 만듬
+from itertools import combinations
+a = [1, 2, 3]
+m = 2
+candidates = list(combinations(a, m))
+
+# DFS/BFS
+from collections import deque
+stack = []
+stack.append()
+stack.pop()
+queue = deque()
+queue.append()
+queue.popleft()
