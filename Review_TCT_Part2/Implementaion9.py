a8aa3f4cfa04712246373d29b7971d7f2ba0e97e
JaeminMok <bamer@naver.com>
2023-04-15 02:25:24 +0900

    Implementation

---
+++ Review_TCT_Part2/Implementaion9.py
@@ -0,0 +1,12 @@
+
+
+
+# 자물쇠와 열쇠
+
+
+def rotation(array):
+    x_length = len(array[0])
+    new_array = [[0]]
+    for i in range(len(array[0])):
+        for j in range(len(array)):
+            arra
