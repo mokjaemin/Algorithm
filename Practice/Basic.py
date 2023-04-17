bc94fb1fe4e5e02083aa542f2939dc8173fc34cf
JaeminMok <bamer@naver.com>
2023-04-17 16:11:53 +0900

    Practicing

---
+++ Practice/Basic.py
@@ -0,0 +1,10 @@
+
+
+
+# 1. 문자열 대소문자 판단
+i = 'a'
+print(i.isalpha())
+print(i.isupper())
+print(i.islower())
+print(i.upper())
+print(i.lower())
--- Test.py
+++
@@ -1,5 +0,0 @@
-
-
-list = [0, 2, 1, 7, 9]
-list.pop(0)
-print(list)
