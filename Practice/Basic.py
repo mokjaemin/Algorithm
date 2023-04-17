b37d05367fcda56bb12dbc0446579b05ef6e5e03
JaeminMok <bamer@naver.com>
2023-04-17 19:18:10 +0900

    Practicing

--- Practice/Basic.py
+++ Practice/Basic.py
@@ -8,3 +8,12 @@ print(i.isupper())
 print(i.islower())
 print(i.upper())
 print(i.lower())
+
+
+# 2. 리스트를 문자열로
+check = ['A', 'B']
+answer = ''.join(str(s) for s in check)
+
+# 3. 특정 인덱스에 삽입, 인덱스 0에 0삽입
+check = [1, 2, 3, 4, 5]
+check.insert(0, 0)
