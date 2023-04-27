b1211e3d37caceadc11654004cfcbb6260d72f79
JaeminMok <bamer@naver.com>
2023-04-27 14:30:13 +0900

    Hash

---
+++ Programmers/Level1/Hash_Counter.py
@@ -0,0 +1,16 @@
+
+
+# 해당 리스트에 어떤 요소가 몇개 있는지 해시(키-값) 만들어 줌.
+
+from collections import Counter
+
+def solution(participant, completion):
+    answer = ''
+    p_dict = Counter(participant)
+    c_dict = Counter(completion)
+    
+    for p_name in p_dict:
+        if p_dict[p_name] != c_dict[p_name]:
+            answer = p_name
+
+    return answer
