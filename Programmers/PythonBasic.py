6ca5906719e8d4bab1dccc79c57955f6930e31c4
JaeminMok <bamer@naver.com>
2023-04-29 23:28:38 +0900

    JAVA..

--- Programmers/PythonBasic.py
+++ Programmers/PythonBasic.py
@@ -17,3 +17,19 @@ answer = ''.join(str(s) for s in check)
 # 3. 특정 인덱스에 삽입, 인덱스 0에 0삽입
 check = [1, 2, 3, 4, 5]
 check.insert(0, 0)
+
+# 4. 반올림, 2번째 자리에서 반올림, 반내림, 올림
+import math
+round(2.14)
+round(2.14, 2)
+math.floor(2.14)
+math.ceil(2.14)
+
+
+# 5. 큐
+from collections import deque
+queue = deque()
+
+# 6. 리스트의 특정 값 제거
+listA = [1, 2, 3, 4, 5]
+listA.remove(3)
