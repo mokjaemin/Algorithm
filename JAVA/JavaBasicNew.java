49e85189cc2c22bbb1184bc8ff9f3fa6a890b8bb
JaeminMok <bamer@naver.com>
2023-06-25 00:47:01 +0900

    Refactoring

--- JAVA/JavaBasicNew.java
+++ JAVA/JavaBasicNew.java
@@ -355,7 +355,7 @@ public class JavaBasicNew {
         minHeap.add(1); // 추가
         minHeap.peek(); // 최솟값 출력
         minHeap.poll(); // 최솟값 제거
-        minHeap.remove(1);
+        // minHeap.remove(1);
 
 
         // 우선순위 큐 (최대힙)
