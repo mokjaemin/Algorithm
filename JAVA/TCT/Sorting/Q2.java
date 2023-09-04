b0e69edcd0644b5f6e870dcdc5c00444cd1fc4ac
JaeminMok <bamer@naver.com>
2023-09-04 16:14:54 +0900

    Sorting

--- JAVA/TCT/Sorting/Q2.java
+++ JAVA/TCT/Sorting/Q2.java
@@ -6,6 +6,13 @@ import java.util.*;
 public class Q2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-    
+        int n = sc.nextInt();
+        List<Integer> values = new ArrayList<>();
+        for(int i=0; i<n; i++){
+            int now = sc.nextInt();
+            values.add(now);
+        }
+        Collections.sort(values);
+        System.out.println(values.get((n-1)/2));
     }
 }
