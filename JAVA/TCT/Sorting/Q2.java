99db87775eb80bdee7c31104dfa2a65084323224
JaeminMok <bamer@naver.com>
2023-09-04 15:27:14 +0900

    testing

---
+++ JAVA/TCT/Sorting/Q2.java
@@ -0,0 +1,11 @@
+package JAVA.TCT.Sorting;
+
+import java.util.*;
+
+// 안테나
+public class Q2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+    
+    }
+}
