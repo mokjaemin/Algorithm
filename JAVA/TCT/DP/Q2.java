69305e9302734077c4bf0e3219ad05a13f193bdb
JaeminMok <bamer@naver.com>
2023-09-12 23:41:10 +0900

    git add . git commit -m "test" git push origin main

--- JAVA/TCT/DP/Q2.java
+++ JAVA/TCT/DP/Q2.java
@@ -2,6 +2,7 @@ package JAVA.TCT.DP;
 
 import java.util.Scanner;
 
+
 // 정수 삼각형
 public class Q2 {
     public static void main(String[] args){
@@ -15,22 +16,28 @@ public class Q2 {
             }
             count += 1;
         }
-        count = 2;
-        int max_value = 0;
-        for(int i=1; i<n; i++){
-            for(int j=0; j<count; j++){
-                int now = 0;
-                now = Math.max(now, map[i-1][j]);
-                if(j-1 >= 0){
-                    now = Math.max(now, map[i-1][j-1]);
-                }
-                map[i][j] += now;
-                if(i == n-1){
-                    max_value = Math.max(map[i][j], max_value);
-                }
-            }
-            count += 1;
+        if(n == 1){
+            System.out.println(map[0][0]);
         }
-        System.out.println(max_value);
+        else{
+            count = 2;
+            int max_value = 0;
+            for(int i=1; i<n; i++){
+                for(int j=0; j<count; j++){
+                    int now = 0;
+                    now = Math.max(now, map[i-1][j]);
+                    if(j-1 >= 0){
+                        now = Math.max(now, map[i-1][j-1]);
+                    }
+                    map[i][j] += now;
+                    if(i == n-1){
+                        max_value = Math.max(map[i][j], max_value);
+                    }
+                }
+                count += 1;
+            }
+            System.out.println(max_value);
+        }
+
     }
 }
