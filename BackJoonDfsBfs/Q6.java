ac2b1f5333c531900fa1608edaa4775e61cc36d1
JaeminMok <bamer@naver.com>
2023-11-01 11:10:12 +0900

    UPDATE

--- BackJoonDfsBfs/Q6.java
+++ BackJoonDfsBfs/Q6.java
@@ -10,13 +10,13 @@ public class Q6 {
         int m = sc.nextInt();
         int n = sc.nextInt();
         int h = sc.nextInt();
-        int[][][] map = new int[n][m][h];
+        int[][][] map = new int[h][n][m];
         List<List<Integer>> queue = new ArrayList<>();
         for(int k=0; k<h; k++){
             for(int i=0; i<n; i++){
                 for(int j=0; j<m; j++){
-                    map[i][j][k] = sc.nextInt();
-                    if(map[i][j][k] == 1){
+                    map[k][i][j] = sc.nextInt();
+                    if(map[k][i][j] == 1 && queue.size() == 0){
                         queue.add(Arrays.asList(i, j, k));
                     }
                 }
@@ -26,14 +26,14 @@ public class Q6 {
         int[] dy = {0, -1, 0, 1, 0, 0};
         int[] dz = {0, 0, 0, 0, -1, 1};
         int time = 0;
-        boolean[][][] visited = new boolean[n][m][h];
+        boolean[][][] visited = new boolean[h][n][m];
         while(queue.size() > 0){
             List<Integer> now = queue.remove(0);
             int x = now.get(0);
             int y = now.get(1);
             int z = now.get(2);
-            int value = map[x][y][z];
-            if(visited[x][y][z]){
+            int value = map[z][x][y];
+            if(visited[z][x][y]){
                 continue;
             }
             visited[x][y][z] = true;
@@ -45,16 +45,16 @@ public class Q6 {
                 if(next_x < 0 | next_x >= n | next_y < 0 | next_y >= m | next_z < 0 | next_z >= h){
                     continue;
                 }
-                if(visited[next_x][next_y][next_z]){
+                if(visited[next_z][next_x][next_y]){
                     continue;
                 }
-                if(map[next_x][next_y][next_z] != 0){
+                if(map[next_z][next_x][next_y] == -1){
                     continue;
                 }
                 // 덜 익은, 익은 토마토
-                queue.add(Arrays.asList(next_x, next_y, next_z));
-                if(value != map[next_x][next_y][next_z]){
-                    map[next_x][next_y][next_z] = value + 1;
+                queue.add(Arrays.asList(next_z, next_x, next_y));
+                if(value != map[next_z][next_x][next_y]){
+                    map[next_z][next_x][next_y] = value + 1;
                 }
             }
         }
